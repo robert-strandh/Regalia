1dd294674ed7b53ef72d767bbc238f04460cbbe3
Robert Strandh <robert.strandh@gmail.com>
2024-08-04 14:11:05 +0200

    New comoponents containg vector classes with unsigned bytes.

---
+++ Code/Class-hierarchy/vector-unsigned-byte-32.lisp
@@ -0,0 +1,4 @@
+(cl:in-package #:regalia)
+
+(defclass vector-unsigned-byte-32 (vector array-unsigned-byte-32)
+  ())
---
+++ Code/Class-hierarchy/vector-unsigned-byte-64.lisp
@@ -0,0 +1,4 @@
+(cl:in-package #:regalia)
+
+(defclass vector-unsigned-byte-64 (vector array-unsigned-byte-64)
+  ())
