8ec3fd1ea84eeefbb5625454ddc6bff4d50d254d
Robert Strandh <robert.strandh@gmail.com>
2024-08-04 14:11:30 +0200

    New comoponents containg vector classes with signed bytes.

---
+++ Code/Class-hierarchy/vector-signed-byte-16.lisp
@@ -0,0 +1,4 @@
+(cl:in-package #:regalia)
+
+(defclass vector-signed-byte-16 (vector array-signed-byte-16)
+  ())
---
+++ Code/Class-hierarchy/vector-signed-byte-32.lisp
@@ -0,0 +1,4 @@
+(cl:in-package #:regalia)
+
+(defclass vector-signed-byte-32 (vector array-signed-byte-32)
+  ())
