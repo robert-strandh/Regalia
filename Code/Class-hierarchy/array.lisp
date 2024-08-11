ef96b82c4481efe60e1b4744ebba37a65dff275c
Robert Strandh <robert.strandh@gmail.com>
2024-08-11 08:07:55 +0200

    Add comment to slot UNDERLYING-ARRAY.

--- Code/Class-hierarchy/array.lisp
+++ Code/Class-hierarchy/array.lisp
@@ -12,6 +12,10 @@
   ((%dimensions
     :initarg :dimensions
     :reader array-dimensions)
+   ;; When this slot contains NIL, then the array is simple.  When the
+   ;; array is a displaced array, then this slot contains the arry to
+   ;; which this array is displaced.  When the array is a non-simple
+   ;; and not displaced, this slot contains a simple array.
    (%underlying-array
     :initform nil
     :initarg :underlying-array
