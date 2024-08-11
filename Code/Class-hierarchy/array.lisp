52ab26a13ca476e0636c7b6ba654cbe5b7db8b97
Robert Strandh <robert.strandh@gmail.com>
2024-08-11 08:06:00 +0200

    Add slot UNDERLYING-ARRAY.

--- Code/Class-hierarchy/array.lisp
+++ Code/Class-hierarchy/array.lisp
@@ -4,7 +4,15 @@
 ;;; of dimensions.
 (defgeneric array-dimensions (array))
 
+(defgeneric underlying-array (array))
+
 ;;; ARRAY is a standard class, so STANDARD-OBJECT will automatically
 ;;; be included as a superclass.
 (defclass array ()
-  ((%dimensions :initarg :dimensions :reader array-dimensions)))
+  ((%dimensions
+    :initarg :dimensions
+    :reader array-dimensions)
+   (%underlying-array
+    :initform nil
+    :initarg :underlying-array
+    :accessor underlying-array)))
