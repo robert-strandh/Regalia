7cb7d1d7678a9cdfd2cb2f689f00d8de0120322c
Robert Strandh <robert.strandh@gmail.com>
2024-08-11 08:02:49 +0200

    Shadow and export standard symbols.

--- Code/Base-extrinsic/packages.lisp
+++ Code/Base-extrinsic/packages.lisp
@@ -2,5 +2,43 @@
 
 (defpackage #:regalia
   (:use #:common-lisp)
-  (:shadow #:array #:array-dimensions)
-  (:export #:array))
+  (:shadow
+   . #1=(#:array
+         #:simple-array
+         #:vector
+         #:simple-vector
+         #:bit-vector
+         #:simple-bit-vector
+         #:make-array
+         #:adjust-array
+         #:adjustable-array-p
+         #:aref
+         #:array-dimension
+         #:array-dimensions
+         #:array-element-type
+         #:array-has-fill-pointer-p
+         #:array-displacement
+         #:array-in-bounds-p
+         #:array-rank
+         #:array-row-major-index
+         #:array-total-size
+         #:arrayp
+         #:fill-pointer
+         #:row-marjor-aref
+         #:upgraded-array-element-type
+         #:array-dimensions-limit
+         #:array-rank-limit
+         #:array-total-size-limit
+         #:simple-vector-p
+         #:svref
+         #:vector
+         #:vector-pop
+         #:vector-push
+         #:vector-push-extend
+         #:vectorp
+         #:bit
+         #:sbit
+         #:bit-not
+         #:bit-vector-p
+         #:simple-bit-vector-p))
+  (:export . #1#))
