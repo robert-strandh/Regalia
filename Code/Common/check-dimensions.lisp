f07ec5470f8536f870df59ea40ad1ae137498baa
Robert Strandh <robert.strandh@gmail.com>
2024-08-14 09:15:56 +0200

    Check against ARRAY-DIMENSION-LIMIT and ARRAY-TOTAL-SIZE-LIMIT.

--- Code/Common/check-dimensions.lisp
+++ Code/Common/check-dimensions.lisp
@@ -4,13 +4,14 @@
   (integerp (ignore-errors (list-length list))))
 
 (defun valid-dimension-p (dimension)
-  (typep dimension '(integer 0)))
+  (typep dimension `(integer 0 (,array-dimension-limit))))
 
 (defun check-and-canonicalize-dimensions (dimensions)
   (cond ((valid-dimension-p dimensions)
          (list dimensions))
         ((and (proper-list-p dimensions)
-              (every #'valid-dimension-p dimensions))
+              (every #'valid-dimension-p dimensions)
+              (< (apply #'* dimensions) array-total-size-limit))
          dimensions)
         (t
          (error 'invalid-dimensions
