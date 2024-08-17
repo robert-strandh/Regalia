18034ff642325a5241ed8a33a6090b398a7c1d5e
Robert Strandh <robert.strandh@gmail.com>
2024-08-17 16:15:53 +0200

    Define MAKE-ARRAY.

--- Code/Common/make-array.lisp
+++ Code/Common/make-array.lisp
@@ -42,39 +42,41 @@
   (loop for i from 0 below size
         do (setf (aref array i) element)))
 
-(defun fill-array-from-contents (dimensions contents step index array)
-  (if (null dimensions)
-      (setf (row-major-aref array index) contents)
-      (if (listp contents)
-          (let ((length (ignore-errors (list-length contents))))
-            (cond ((null length)
-                   ;; FIXME: signal a more specific error.
-                   (error "contents must be proper list"))
-                  ((not (= length (first dimensions)))
-                   ;; FIXME: signal a more specific error.
-                   (error "incorrect contents length"))
-                  (t 
-                   (loop for i from index by step
-                         for sequence-or-element in contents
-                         do (foo (rest dimensions)
-                                 sequence-or-element
-                                 (/ step (first dimensions))
-                                 i
-                                 array)))))
-          (let ((length (length contents)))
-            (if (not (= length (first dimensions)))
-                ;; FIXME: signal a more specific error.
-                (error "incorrect contents length")
-                (loop for i from index by step
-                      for sequence-or-element across contents
-                      do (foo (rest dimensions)
-                              sequence-or-element
-                              (/ step (first dimensions))
-                              i
-                              array)))))))
+
+(defun fill-array-from-contents (array dimensions contents)
+  (labels ((aux (dimensions contents step index)
+             (if (null dimensions)
+                 (setf (row-major-aref array index) contents)
+                 (if (listp contents)
+                     (let ((length (ignore-errors (list-length contents))))
+                       (cond ((null length)
+                              ;; FIXME: signal a more specific error.
+                              (error "contents must be proper list"))
+                             ((not (= length (first dimensions)))
+                              ;; FIXME: signal a more specific error.
+                              (error "incorrect contents length"))
+                             (t 
+                              (loop for i from index by step
+                                    for sequence-or-element in contents
+                                    do (aux (rest dimensions)
+                                            sequence-or-element
+                                            (/ step (first dimensions))
+                                            i)))))
+                     (let ((length (length contents)))
+                       (if (not (= length (first dimensions)))
+                           ;; FIXME: signal a more specific error.
+                           (error "incorrect contents length")
+                           (loop for i from index by step
+                                 for sequence-or-element across contents
+                                 do (aux (rest dimensions)
+                                         sequence-or-element
+                                         (/ step (first dimensions))
+                                         i))))))))
+    (aux dimensions contents (apply #'* (butlast dimensions)) 0)))
 
 (defun make-array
     (dimensions
+     &rest arguments
      &key
        (element-type t)
        (initial-element nil initial-element-p)
@@ -84,25 +86,54 @@
        displaced-to
        (displaced-index-offset 0 displaced-index-offset-p)
        environment)
-  (let ((class-name
-          (if (= (length canonicalized-dimensions) 1)
-              (vector-class-name-from-element-type
-               element-type environment)
-              (non-vector-class-name-from-element-type
-               element-type environment))))
-    (if (not (null displaced-to))
-        (let ((canonicalized-dimensions
-                (check-and-canonicalize-dimensions dimensions)))
-          (unless (arrayp displaced-to)
-            (error 'object-must-be-an-array
-                   :datum displaced-to
-                   :expected-type 'array))
-          (when initial-element-p
-            (error 'initial-element-cannot-be-given-for-displaced-array))
-          (when initial-contents-p
-            (error 'initial-contents-cannot-be-supplied-for-displaced-array))
-          (make-instance class-name
-            :dimensions canonicalized-dimensions
-            :underlying-array displaced-to
-            :displaced-index-offset displaced-index-offset)))))
-
+  (let* ((canonicalized-dimensions
+           (check-and-canonicalize-dimensions dimensions))
+         (class-name
+           (if (= (length canonicalized-dimensions) 1)
+               (vector-class-name-from-element-type
+                element-type environment)
+               (non-vector-class-name-from-element-type
+                element-type environment))))
+    (cond ((not (null displaced-to))
+           (unless (arrayp displaced-to)
+             (error 'object-must-be-an-array
+                    :datum displaced-to
+                    :expected-type 'array))
+           (when initial-element-p
+             (error 'initial-element-cannot-be-given-for-displaced-array))
+           (when initial-contents-p
+             (error 'initial-contents-cannot-be-supplied-for-displaced-array))
+           (make-instance class-name
+             :dimensions canonicalized-dimensions
+             :underlying-array displaced-to
+             :displaced-index-offset displaced-index-offset))
+          (adjustable
+           (when displaced-index-offset-p
+             (error 'displaced-index-offset-supplied-but-not-displaced-to))
+           (let ((underlying-array
+                   (apply #'make-array :adjustable nil arguments)))
+             (make-instance class-name
+               :dimensions canonicalized-dimensions
+               :underlying-array underlying-array)))
+          (t
+           (let ((result (make-instance class-name
+                           :dimensions canonicalized-dimensions)))
+             (cond (initial-element-p
+                    (when initial-contents-p
+                      (error 'initial-contents-and-initial-element-cannot-both-be-supplied))
+                    (fill-array-with-element
+                     result
+                     (reduce #'* canonicalized-dimensions)
+                     initial-element))
+                   (initial-contents-p
+                    (fill-array-from-contents
+                     result dimensions initial-contents))
+                   (t
+                    (let ((element
+                            (default-initial-element
+                             element-type environment)))
+                      (fill-array-with-element
+                       result
+                       (reduce #'* canonicalized-dimensions)
+                       element))))
+                 result)))))
