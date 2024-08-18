03ccee33f221ae775dbec5fd45d67f02783b301c
Robert Strandh <robert.strandh@gmail.com>
2024-08-18 14:28:22 +0200

    Create the host vector only when UNDERLYING-ARRAY is NIL.

--- Code/Extrinsic/initialize-instance.lisp
+++ Code/Extrinsic/initialize-instance.lisp
@@ -1,6 +1,12 @@
 (cl:in-package #:regalia)
 
 (defmethod initialize-instance :after
-    ((array array) &key size element-type &allow-other-keys)
-  (reinitialize-instance array
-    :contents (cl:make-array size :element-type element-type)))
+    ((array array)
+     &key
+       size
+       element-type
+       underlying-array
+     &allow-other-keys)
+  (when (null underlying-array)
+    (reinitialize-instance array
+      :contents (cl:make-array size :element-type element-type))))
