919c22b89a705d992e3949dec373507e718d836e
Robert Strandh <robert.strandh@gmail.com>
2024-08-18 15:01:20 +0200

    Call host AREF rather than host SVREF.

--- Code/Base-extrinsic/accessors.lisp
+++ Code/Base-extrinsic/accessors.lisp
@@ -3,82 +3,82 @@
 (defgeneric contents (array))
 
 (defun t-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf t-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun signed-64-bit-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf signed-64-bit-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun unsigned-64-bit-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf unsigned-64-bit-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun signed-32-bit-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf signed-32-bit-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun unsigned-32-bit-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf unsigned-32-bit-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun signed-16-bit-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf signed-16-bit-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun unsigned-16-bit-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf unsigned-16-bit-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun signed-8-bit-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf signed-8-bit-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun unsigned-8-bit-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf unsigned-8-bit-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun single-float-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf single-float-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun double-float-aref (array index)
-  (cl:svref (contents array) index))
+  (cl:aref (contents array) index))
 
 (defun (setf double-float-aref) (new-element array index)
-  (setf (cl:svref (contents array) index) new-element))
+  (setf (cl:aref (contents array) index) new-element))
 
 (defun complex-single-float-aref (array index)
-  (cl:svref (contents array) (* 2 index)))
+  (cl:aref (contents array) (* 2 index)))
 
 (defun (setf complex-single-float-aref) (new-element array index)
-  (setf (cl:svref (contents array) (* 2 index)) new-element))
+  (setf (cl:aref (contents array) (* 2 index)) new-element))
 
 (defun complex-double-float-aref (array index)
-  (cl:svref (contents array) (* 2 index)))
+  (cl:aref (contents array) (* 2 index)))
 
 (defun (setf complex-double-float-aref) (new-element array index)
-  (setf (cl:svref (contents array) (* 2 index)) new-element))
+  (setf (cl:aref (contents array) (* 2 index)) new-element))
 
 (defun character-aref (array index)
   (cl:schar array index))
