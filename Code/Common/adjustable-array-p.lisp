d608e5e36f8bd3559154d5b1e5aed27daf24c78f
Robert Strandh <robert.strandh@gmail.com>
2024-08-21 16:36:14 +0200

    Provide documentation string for ADJUSTABLE-ARRAY-P.

--- Code/Common/adjustable-array-p.lisp
+++ Code/Common/adjustable-array-p.lisp
@@ -6,3 +6,15 @@
            :datum array
            :expected-type 'array))
   (not (null (underlying-array array))))
+
+(setf (documentation 'adjustable-array-p 'function)
+      (format nil
+              "Syntax: adjustable-array-p array~@
+               ~@
+               This function returns T if ARRAY is an actually adjustable~@
+               array.  It returns NIL if ARRAY is an array that is not~@
+               actually adjustable.~@
+               ~@
+               An error of type TYPE-ERROR is signaled if ARRAY is an~@
+               object other than an array."))
+
