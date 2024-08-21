740f8be1ac37c1b4403dd02d375b22507c95c838
Robert Strandh <robert.strandh@gmail.com>
2024-08-21 16:36:00 +0200

    Provide documentation string for ARRAYP.

--- Code/Common/arrayp.lisp
+++ Code/Common/arrayp.lisp
@@ -4,3 +4,9 @@
   (:method (object) nil)
   (:method ((object array)) t))
 
+(setf (documentation 'arrayp 'function)
+      (format nil
+              "Syntax: arrayp object~@
+               ~@
+               This function returns T if OBJECT is of type ARRAY.~@
+               Otherwise it return NIL."))
