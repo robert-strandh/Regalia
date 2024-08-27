92fa415d110bbb68c6dba5efb352bcce31f00d70
Robert Strandh <robert.strandh@gmail.com>
2024-08-27 14:20:22 +0200

    Define default method on ARRAY-DIMENSIONS.

--- Code/Common/dimensions.lisp
+++ Code/Common/dimensions.lisp
@@ -1,5 +1,11 @@
 (cl:in-package #:regalia)
 
+;;; This method is applicable when ARRAY-DIMENSIONS is given an object
+;;; that is not an array.  The standard requires us to signal a type
+;;; error then.
+(defmethod array-dimensions (array)
+  (error 'type-error :datum array :expected-type 'array))
+
 (defmethod array-dimensions :around ((array array))
   (if (or (array-is-simple-p array)
           (array-is-displaced-p array))
