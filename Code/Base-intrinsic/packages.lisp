1fa29a720d306f549f01a583f0a35755c0ef18d5
Robert Strandh <robert.strandh@gmail.com>
2024-12-13 13:41:46 +0100

    Export names of low-level accessors.

--- Code/Base-intrinsic/packages.lisp
+++ Code/Base-intrinsic/packages.lisp
@@ -2,4 +2,20 @@
 
 (defpackage #:regalia
   (:use #:common-lisp)
-  (:export #:make-array-instance)
+  (:export
+   #:make-array-instance
+   #:t-aref
+   #:signed-64-bit-aref
+   #:unsigned-64-bit-aref
+   #:signed-32-bit-aref
+   #:unsigned-32-bit-aref
+   #:signed-16-bit-aref
+   #:unsigned-16-bit-aref
+   #:signed-8-bit-aref
+   #:unsigned-8-bit-aref
+   #:single-float-aref
+   #:double-float-aref
+   #:complex-single-float-aref
+   #:complex-double-float-aref
+   #:character-aref
+   #:bit-aref))
